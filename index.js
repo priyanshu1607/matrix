8fa68583baabb13843fb576aabc0286d437b4627
priyanshu1607 <priyanshumathur215@gmail.com>
2022-07-25 17:50:22 +0530

    image changes, timmer changed, footer added

--- index.js
+++ index.js
@@ -1,39 +1,33 @@
 
-// Set the date we're counting down to
-var countDownDate = new Date("July 25, 2022 15:37:25").getTime();
+let daysItem = document.querySelector("#days");
+let hoursItem = document.querySelector("#hours");
+let minItem = document.querySelector("#min");
+let secItem = document.querySelector("#sec");
 
-// Update the count down every 1 second
-var x = setInterval(function() {
 
-  // Get today's date and time
-  var now = new Date().getTime();
-    
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-    
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="Timer"
-  if(days==0){
-    document.getElementById("Timer").innerHTML = hours + "h "
-    + minutes + "m " + seconds + "s "
-  }
-else{  
-    document.getElementById("Timer").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";}
+let countDown = () => {
+  let futureDate = new Date("1 Jan 2023");
+  let currentDate = new Date();
+  let myDate = futureDate - currentDate;
+  //console.log(myDate);
 
-    
-  // If the count down is over, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("Timer").innerHTML = "contest started";
-  }
-}, 1000);
+  let days = Math.floor(myDate / 1000 / 60 / 60 / 24);
 
+  let hours = Math.floor(myDate / 1000 / 60 / 60) % 24;
+
+  let min = Math.floor(myDate / 1000 / 60) % 60;
+
+  let sec = Math.floor(myDate / 1000) % 60;
+
+  daysItem.innerHTML = days;
+  hoursItem.innerHTML = hours;
+  minItem.innerHTML = min;
+  secItem.innerHTML = sec;
+}
+
+countDown()
+
+setInterval(countDown, 1000)
 
 // NAV BAR SCROLLING 
 
@@ -68,3 +62,5 @@ else{
 
    }
  });
+
+ 
